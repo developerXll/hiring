f71b82fa746c5beeaa0afd0a1bd71adceb074aa4
longlong.xie <longlong.xie@argylesoftware.co.uk>
2016-05-04 16:06:07 +0800

    compile code

--- src/main/java/com/hiring/dao/impl/BaseDaoImpl.java
+++ src/main/java/com/hiring/dao/impl/BaseDaoImpl.java
@@ -12,9 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.hiring.dao.BaseDao;
 import com.hiring.framework.Page;
 
-public class BaseDaoImpl<T, PK extends java.io.Serializable> implements
-		BaseDao<T, PK>
-{
+public class BaseDaoImpl<T, PK extends java.io.Serializable>
+		implements BaseDao<T, PK>
+	{
 
 	@Autowired
 	private SessionFactory sessionFactory;
@@ -23,94 +23,93 @@ public class BaseDaoImpl<T, PK extends java.io.Serializable> implements
 
 	@Override
 	public T get(Serializable id)
-	{
+		{
 		T obj = (T) this.getSession().get(getEntityClass(), id);
 		return obj;
-	}
+		}
 
 	@Override
 	public void save(Object newInstance)
-	{
+		{
 		Serializable id = this.getSession().save(newInstance);
 		System.out.println("save=====" + id);
-		
 
-	}
+		}
 
 	@Override
 	public void delete(Object transientObject)
-	{
+		{
 		this.getSession().delete(transientObject);
 
-	}
+		}
 
 	@Override
 	public void deleteById(Serializable id)
-	{
+		{
 		T t = get(id);
 		this.getSession().delete(t);
 
-	}
+		}
 
 	@Override
 	public void update(T t)
-	{
+		{
 		this.getSession().update(t);
 
-	}
+		}
 
 	public SessionFactory getSessionFactory()
-	{
+		{
 		return sessionFactory;
-	}
+		}
 
 	public Session getSession()
-	{
+		{
 		// 需要开启事物，才能得到CurrentSession
 		return sessionFactory.getCurrentSession();
-	}
+		}
 
 	public void setSessionFactory(SessionFactory sessionFactory)
-	{
-		this.sessionFactory = sessionFactory;
-	}
-
-	@SuppressWarnings (
-	{ "unchecked" })
-	protected Class<T> getEntityClass()
-	{
-		if (entityClass == null)
 		{
+		this.sessionFactory = sessionFactory;
+		}
+
+	@SuppressWarnings(
+		{ "unchecked" })
+	protected Class<T> getEntityClass()
+		{
+		if (entityClass == null)
+			{
 			entityClass = (Class<T>) ((ParameterizedType) getClass()
 					.getGenericSuperclass()).getActualTypeArguments()[0];
-		}
+			}
 		return entityClass;
-	}
+		}
 
 	@Override
 	public T load(PK pk)
-	{
+		{
 		T obj = (T) this.getSession().load(getEntityClass(), pk);
 		return obj;
-	}
+		}
 
-	@SuppressWarnings ("unchecked")
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<T> getByPage(Page page)
-	{
+		{
 		Criteria criteria = this.getSession().createCriteria(getEntityClass());
 		criteria.setFirstResult((page.getPageNo() - 1) * page.getPageSize());
 		criteria.setMaxResults(page.getPageSize());
 		return criteria.list();
-	}
+		}
 
-	@SuppressWarnings ("rawtypes")
+	@SuppressWarnings("rawtypes")
 	@Override
 	public int getCount()
-	{
+		{
 		Criteria criteria = this.getSession().createCriteria(getEntityClass());
 		List list = criteria.list();
 		return list != null ? list.size() : 0;
-	}
+		}
 
-}
+	}
